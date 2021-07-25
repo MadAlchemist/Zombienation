c8857938d330c84376de3db200088ed1aaeefe08
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-07-25 20:46:44 +0300

    Create Zombie_1_Renderer.java

---
+++ src/main/java/com/madalchemist/betterzombies/zombies/Zombie_1_Renderer.java
@@ -0,0 +1,40 @@
+package com.madalchemist.betterzombies.zombies;
+
+import net.minecraft.client.renderer.entity.AbstractZombieRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.ZombieRenderer;
+import net.minecraft.client.renderer.entity.layers.BipedArmorLayer;
+import net.minecraft.client.renderer.entity.layers.HeldItemLayer;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.entity.model.ZombieModel;
+import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nonnull;
+
+@OnlyIn(Dist.CLIENT)
+public class Zombie_1_Renderer extends AbstractZombieRenderer<Zombie1,ZombieModel<Zombie1>> {
+
+    public Zombie_1_Renderer(EntityRendererManager rendererManager) {
+        super(rendererManager,
+              new ZombieModel<>(0.0f, false),
+              new ZombieModel<>(0.5f, true),
+              new ZombieModel<>(1.0f, true));
+
+        this.addLayer(new BipedArmorLayer<>(this, new BipedModel(0.5F), new BipedModel(1.0F)));
+        this.addLayer(new HeldItemLayer<>(this));
+    }
+
+    @Nonnull
+    public ResourceLocation getTextureLocation(@Nonnull Zombie1 zombie) {
+        return this.getTexture("zombie_1");
+    }
+
+    private ResourceLocation getTexture(String fileName) {
+        return new ResourceLocation("betterzombies",
+                                    "textures/entity/zombie/" + fileName + ".png");
+    }
+}
