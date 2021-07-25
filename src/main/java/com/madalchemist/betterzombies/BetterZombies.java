df9048b7988442c052e524f0258eb8fca6183a05
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-07-25 20:45:40 +0300

    Update BetterZombies.java

--- src/main/java/com/madalchemist/betterzombies/BetterZombies.java
+++ src/main/java/com/madalchemist/betterzombies/BetterZombies.java
@@ -1,14 +1,16 @@
 package com.madalchemist.betterzombies;
 
+import com.madalchemist.betterzombies.zombies.*;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.madalchemist.betterzombies.zombies.BasicZombieRenderer;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("betterzombies")
@@ -21,6 +23,7 @@ public class BetterZombies
         eventBus.addListener(this::setupCommon);
         eventBus.addListener(this::setupClient);
 
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.spec);
         //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigurationHandler.spec);
         registerDeferredRegistries(eventBus);
     }
@@ -29,7 +32,14 @@ public class BetterZombies
     }
 
     public void setupClient(final FMLClientSetupEvent event) {
-        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.BASIC_ZOMBIE.get(), BasicZombieRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_1.get(), Zombie_1_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_2.get(), Zombie_2_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_3.get(), Zombie_3_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_4.get(), Zombie_4_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_5.get(), Zombie_5_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_6.get(), Zombie_6_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_7.get(), Zombie_7_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_8.get(), Zombie_8_Renderer::new);
     }
 
     public static void registerDeferredRegistries(IEventBus modBus) {
