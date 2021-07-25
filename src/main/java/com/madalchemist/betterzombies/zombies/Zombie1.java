712dc95522142404244a53f3393a39f27dc6a7c5
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-07-25 20:46:16 +0300

    Create Zombie1.java

---
+++ src/main/java/com/madalchemist/betterzombies/zombies/Zombie1.java
@@ -0,0 +1,52 @@
+package com.madalchemist.betterzombies.zombies;
+
+import com.madalchemist.betterzombies.ConfigHandler;
+import com.madalchemist.betterzombies.SoundsRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.IServerWorld;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+
+public class Zombie1 extends ZombieEntity {
+
+    public Zombie1(EntityType<? extends Zombie1> zombie, World world) {
+        super(zombie, world);
+    }
+
+    @Override
+    protected boolean isSunSensitive() {
+        return ConfigHandler.GENERAL.burnAtDay.get();
+    }
+
+    protected SoundEvent getAmbientSound() {
+        return SoundsRegistry.ZOMBIE1_AMBIENT;
+    }
+    protected SoundEvent getHurtSound(DamageSource p_184601_1_) {
+        return SoundEvents.ZOMBIE_HURT;
+    }
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ZOMBIE_DEATH;
+    }
+    protected SoundEvent getStepSound() {
+        return SoundEvents.ZOMBIE_STEP;
+    }
+    protected void playStepSound(BlockPos p_180429_1_, BlockState p_180429_2_) {
+        this.playSound(this.getStepSound(), 0.15F, 1.0F);
+    }
+
+    public boolean checkSpawnRules(IServerWorld world, SpawnReason reason) {
+       return super.checkSpawnRules(world, reason);
+    }
+}
