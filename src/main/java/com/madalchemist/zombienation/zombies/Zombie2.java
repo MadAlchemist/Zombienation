ed76e2e868fe06eb6e6997f5879fc7f0e080236b
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-07-26 17:16:25 +0300

    Create Zombie2.java

---
+++ src/main/java/com/madalchemist/zombienation/zombies/Zombie2.java
@@ -0,0 +1,41 @@
+package com.madalchemist.zombienation.zombies;
+
+import com.madalchemist.zombienation.ConfigHandler;
+import com.madalchemist.zombienation.SoundsRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+
+public class Zombie2 extends ZombieEntity {
+
+    public Zombie2(EntityType<? extends Zombie2> zombie, World world) {
+        super(zombie, world);
+    }
+
+    @Override
+    protected boolean isSunSensitive() {
+        return ConfigHandler.GENERAL.burnAtDay.get();
+    }
+
+    protected SoundEvent getAmbientSound() {
+        return SoundsRegistry.ZOMBIE2_AMBIENT;
+    }
+    protected SoundEvent getHurtSound(DamageSource p_184601_1_) {
+        return SoundEvents.ZOMBIE_HURT;
+    }
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ZOMBIE_DEATH;
+    }
+    protected SoundEvent getStepSound() {
+        return SoundEvents.ZOMBIE_STEP;
+    }
+    protected void playStepSound(BlockPos p_180429_1_, BlockState p_180429_2_) {
+        this.playSound(this.getStepSound(), 0.15F, 1.0F);
+    }
+}
