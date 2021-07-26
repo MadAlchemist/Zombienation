05e584f136502a0fe3fa6447c97c5276babc70df
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-07-26 17:16:19 +0300

    Create Zombienation.java

---
+++ src/main/java/com/madalchemist/zombienation/Zombienation.java
@@ -0,0 +1,48 @@
+package com.madalchemist.zombienation;
+
+import com.madalchemist.zombienation.zombies.*;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("zombienation")
+public class Zombienation
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+    public Zombienation() {
+        final IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setupCommon);
+        eventBus.addListener(this::setupClient);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.spec);
+        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigurationHandler.spec);
+        registerDeferredRegistries(eventBus);
+    }
+    public void setupCommon(final FMLCommonSetupEvent event) {
+
+    }
+
+    public void setupClient(final FMLClientSetupEvent event) {
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_1.get(), Zombie_1_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_2.get(), Zombie_2_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_3.get(), Zombie_3_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_4.get(), Zombie_4_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_5.get(), Zombie_5_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_6.get(), Zombie_6_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_7.get(), Zombie_7_Renderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(ZombiesRegistry.ZOMBIE_8.get(), Zombie_8_Renderer::new);
+    }
+
+    public static void registerDeferredRegistries(IEventBus modBus) {
+        ZombiesRegistry.ENTITY_DEFERRED.register(modBus);
+    }
+}
