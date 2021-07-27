770e3671ad6be8e77f7a1458fb9091c994b5f592
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-07-27 23:12:59 +0300

    Update ConfigHandler.java

--- src/main/java/com/madalchemist/zombienation/ConfigHandler.java
+++ src/main/java/com/madalchemist/zombienation/ConfigHandler.java
@@ -13,6 +13,7 @@ public class ConfigHandler {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final General GENERAL = new General(BUILDER);
     public static final Spawn SPAWN = new Spawn(BUILDER);
+    public static Infection INFECTION = new Infection(BUILDER);
 
     public static class General {
         public final ForgeConfigSpec.BooleanValue burnAtDay;
@@ -23,7 +24,7 @@ public class ConfigHandler {
         General(ForgeConfigSpec.Builder builder) {
             builder.push("General");
             burnAtDay = builder
-                    .comment("Do modded zombies burn at day?")
+                    .comment("Do modded zombies burn at day (not affects vanilla zombies)?")
                     .define("burnAtDay", false);
             warriorsHaveSwords = builder
                     .comment("Do zombie warriors have enchanted iron swords?")
@@ -34,11 +35,46 @@ public class ConfigHandler {
             hazmatSuitProtectsFromPotions = builder
                     .comment("Are hazmat zombies immune to potions?")
                     .define("hazmatSuitProtectsFromPotions", true);
+
             builder.pop();
         }
 
 
     }
+
+    public static class Infection {
+        public final ForgeConfigSpec.DoubleValue infectionChance;
+        public final ForgeConfigSpec.IntValue infectionDuration;
+        public final ForgeConfigSpec.BooleanValue infectionHunger;
+        public final ForgeConfigSpec.BooleanValue infectionNausea;
+        public final ForgeConfigSpec.BooleanValue infectionWeakness;
+        public final ForgeConfigSpec.BooleanValue infectionDeathZombification;
+
+
+        Infection(ForgeConfigSpec.Builder builder) {
+            builder.push("Infection");
+            infectionChance = builder
+                    .comment("Infection chance from being hit by zombie, min: 0.0, max: 1.0, default: 0.1")
+                    .defineInRange("infectionChance", 0.1d, 0.0d, 1.0d);
+            infectionDuration = builder
+                    .comment("Infection duration in seconds before you die:")
+                    .defineInRange("infectionDuration", 3600, 0, Integer.MAX_VALUE);
+            infectionHunger = builder
+                    .comment("Does infection cause hunger?")
+                    .define("infectionHunger",true);
+            infectionNausea = builder
+                    .comment("Does infection cause nausea?")
+                    .define("infectionNausea",true);
+            infectionWeakness = builder
+                    .comment("Does infection cause weakness?")
+                    .define("infectionWeakness",true);
+            infectionDeathZombification = builder
+                    .comment("Spawn vanilla zombie when player dies from infection?")
+                    .define("infectionDeathZombification",true);
+            builder.pop();
+        }
+    }
+
     public static class Spawn {
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> include;
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> exclude;
@@ -51,15 +87,13 @@ public class ConfigHandler {
         public final ForgeConfigSpec.IntValue minGroupTough;
         public final ForgeConfigSpec.IntValue maxGroupTough;
 
-        //private int fix;
-
         Spawn(ForgeConfigSpec.Builder builder) {
             builder.push("Spawn");
             builder.comment("BiomeDictionary types to include");
             include = builder.defineList("include", Collections.singletonList(OVERWORLD.toString()), o -> BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(String.valueOf(o))));
             builder.comment("BiomeDictionary types to exclude (overrides \"include\")");
             exclude = builder.defineList("exclude", Arrays.asList(NETHER.toString(), END.toString()), o -> BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(String.valueOf(o))));
-            //fix=100;
+
             spawnWeightNormal = builder
                     .comment("Normal zombie spawn weight (0-1000, vanilla zombies == 100")
                     .defineInRange("spawnWeightNormal", 100,0,1000);
