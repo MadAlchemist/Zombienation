91b6c14478dc7fc4081649fefa1f124baaeb1733
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-09-12 19:16:40 +0300

    Update PotionZombieVirus.java

--- src/main/java/com/madalchemist/zombienation/potions/PotionZombieVirus.java
+++ src/main/java/com/madalchemist/zombienation/potions/PotionZombieVirus.java
@@ -2,8 +2,14 @@ package com.madalchemist.zombienation.potions;
 
 import com.madalchemist.zombienation.ConfigHandler;
 import com.madalchemist.zombienation.ModItems;
+import com.madalchemist.zombienation.PotionsRegistry;
+import com.madalchemist.zombienation.animals.BrownBearEntity;
 import com.madalchemist.zombienation.items.ItemAntizombine;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.entity.passive.PolarBearEntity;
+import net.minecraft.entity.passive.WolfEntity;
+import net.minecraft.entity.passive.horse.HorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -37,6 +43,25 @@ public class PotionZombieVirus extends Effect {
                 entity.addEffect(new EffectInstance(Effects.CONFUSION, 200, 0, false, false));
             if (!entity.hasEffect(Effects.WEAKNESS) && Math.random() <= 0.333 && ConfigHandler.INFECTION.infectionWeakness.get())
                 entity.addEffect(new EffectInstance(Effects.WEAKNESS, 200, 0, false, false));
+        }
+        // Animals don't die from virus itself, but are very vulnerable due to poisoning.
+        if(entity instanceof PolarBearEntity || entity instanceof BrownBearEntity || entity instanceof HorseEntity || entity instanceof WolfEntity) {
+            if (!entity.hasEffect(Effects.POISON) && Math.random() <= 0.000167819)
+                entity.addEffect(new EffectInstance(Effects.POISON, 40, 0, false, false));
+        }
+
+        //Villagers can be healed by applying weakness and strength at same time
+        if(entity instanceof VillagerEntity ||
+           entity instanceof PolarBearEntity ||
+           entity instanceof BrownBearEntity ||
+           entity instanceof HorseEntity ||
+           entity instanceof WolfEntity) {
+            if(entity.hasEffect(Effects.WEAKNESS) && entity.hasEffect(Effects.DAMAGE_BOOST)) {
+                if(!entity.level.isClientSide()) {
+                    entity.removeEffect(PotionsRegistry.POTION_ZOMBIE_VIRUS);
+                    entity.addEffect(new EffectInstance(Effects.GLOWING, 200, 0, false, false));
+                }
+            }
         }
     }
 
