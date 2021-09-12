764e388dfb8e69b8f47b1e333682ed90f57dec9a
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-09-12 19:17:15 +0300

    Update Zombie9.java

--- src/main/java/com/madalchemist/zombienation/zombies/Zombie9.java
+++ src/main/java/com/madalchemist/zombienation/zombies/Zombie9.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.passive.PolarBearEntity;
+import net.minecraft.entity.passive.horse.HorseEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
@@ -47,5 +48,6 @@ public class Zombie9 extends ZombieEntity {
         super.registerGoals();
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, BrownBearEntity.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, PolarBearEntity.class, true));
+        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, HorseEntity.class, true));
     }
 }
