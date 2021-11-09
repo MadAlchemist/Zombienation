90bf7231ef51e7b19436616e0f73456291c060ad
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-11-09 22:10:59 +0300

    Update ConfigHandler.java

--- src/main/java/com/madalchemist/zombienation/ConfigHandler.java
+++ src/main/java/com/madalchemist/zombienation/ConfigHandler.java
@@ -20,13 +20,120 @@ public class ConfigHandler {
     public static final General GENERAL = new General(BUILDER);
     public static final Spawn SPAWN = new Spawn(BUILDER);
     public static Infection INFECTION = new Infection(BUILDER);
+    public static Senses SENSES = new Senses(BUILDER);
+    public static Loot LOOT = new Loot(BUILDER);
 
-    public static class General {
-        public final ForgeConfigSpec.BooleanValue burnAtDay;
-        public final ForgeConfigSpec.BooleanValue warriorsHaveSwords;
-        public final ForgeConfigSpec.BooleanValue minersHavePickaxes;
-        public final ForgeConfigSpec.BooleanValue hazmatSuitProtectsFromPotions;
+    public static class Loot {
+        public ForgeConfigSpec.ConfigValue<String> zombie1_loot;
+        public ForgeConfigSpec.DoubleValue zombie1_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie2_loot;
+        public ForgeConfigSpec.DoubleValue zombie2_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie3_loot;
+        public ForgeConfigSpec.DoubleValue zombie3_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie4_loot;
+        public ForgeConfigSpec.DoubleValue zombie4_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie5_loot;
+        public ForgeConfigSpec.DoubleValue zombie5_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie6_loot;
+        public ForgeConfigSpec.DoubleValue zombie6_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie7_loot;
+        public ForgeConfigSpec.DoubleValue zombie7_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie8_loot;
+        public ForgeConfigSpec.DoubleValue zombie8_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie9_loot;
+        public ForgeConfigSpec.DoubleValue zombie9_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> zombie_bear_loot;
+        public ForgeConfigSpec.DoubleValue zombie_bear_drop_chance;
+        public ForgeConfigSpec.ConfigValue<String> brown_bear_loot;
+        public ForgeConfigSpec.DoubleValue brown_bear_drop_chance;
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> chestheadLoot;
+
+        Loot(ForgeConfigSpec.Builder builder) {
+            builder.push("Loot");
+            zombie1_loot = builder
+                    .comment("Zombie in short pants loot:")
+                    .define("zombie1_loot", "minecraft:rotten_flesh");
+            zombie1_drop_chance = builder
+                    .comment("Zombie in short pants loot drop chance:")
+                    .defineInRange("zombie1_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie2_loot = builder
+                    .comment("Husk with skin head loot:")
+                    .define("zombie2_loot", "minecraft:rotten_flesh");
+            zombie2_drop_chance = builder
+                    .comment("Husk with skin head loot drop chance:")
+                    .defineInRange("zombie2_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie3_loot = builder
+                    .comment("Zombie miner loot:")
+                    .define("zombie3_loot", "minecraft:rotten_flesh");
+            zombie3_drop_chance = builder
+                    .comment("Zombie miner loot drop chance:")
+                    .defineInRange("zombie3_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie4_loot = builder
+                    .comment("Zombie warrior loot:")
+                    .define("zombie4_loot", "minecraft:rotten_flesh");
+            zombie4_drop_chance = builder
+                    .comment("Zombie warrior loot drop chance:")
+                    .defineInRange("zombie4_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie5_loot = builder
+                    .comment("Hazmat zombie loot:")
+                    .define("zombie5_loot", "minecraft:rotten_flesh");
+            zombie5_drop_chance = builder
+                    .comment("Hazmat zombie loot drop chance:")
+                    .defineInRange("zombie5_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie6_loot = builder
+                    .comment("Zombie Alex loot:")
+                    .define("zombie6_loot", "minecraft:rotten_flesh");
+            zombie6_drop_chance = builder
+                    .comment("Zombie Alex loot drop chance:")
+                    .defineInRange("zombie6_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie7_loot = builder
+                    .comment("Husk girl loot:")
+                    .define("zombie7_loot", "minecraft:rotten_flesh");
+            zombie7_drop_chance = builder
+                    .comment("Husk girl loot drop chance:")
+                    .defineInRange("zombie7_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie8_loot = builder
+                    .comment("Zombie girl with flowers on head loot:")
+                    .define("zombie8_loot", "minecraft:rotten_flesh");
+            zombie8_drop_chance = builder
+                    .comment("Zombie girl with flowers on head loot drop chance:")
+                    .defineInRange("zombie8_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie9_loot = builder
+                    .comment("Frozen lumberjack loot:")
+                    .define("zombie9_loot", "minecraft:rotten_flesh");
+            zombie9_drop_chance = builder
+                    .comment("Frozen lumberjack loot drop chance:")
+                    .defineInRange("zombie9_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            zombie_bear_loot = builder
+                    .comment("Zombie bear loot:")
+                    .define("zombie_bear_loot", "minecraft:rotten_flesh");
+            zombie_bear_drop_chance = builder
+                    .comment("Zombie bear loot drop chance:")
+                    .defineInRange("zombie_bear_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            brown_bear_loot = builder
+                    .comment("Brown bear loot:")
+                    .define("brown_bear_loot", "minecraft:leather");
+            brown_bear_drop_chance = builder
+                    .comment("Brown bear loot drop chance:")
+                    .defineInRange("brown_bear_drop_chance", 0.5d, 0.0d, 1.0d);
+
+            chestheadLoot = builder
+                    .comment("Possible chesthead loot")
+                    .defineList("chestheadLoot", new ArrayList<String>(), o -> itemExists(o.toString()));
+
+
+            builder.pop();
+        }
 
         public static boolean itemExists(String ID) {
             String id_parts[] = ID.split(":");
@@ -34,6 +141,13 @@ public class ConfigHandler {
             if(ForgeRegistries.ITEMS.getValue(new ResourceLocation(id_parts[0], id_parts[1])) == null) {return false;}
             return true;
         }
+    }
+
+    public static class General {
+        public final ForgeConfigSpec.BooleanValue burnAtDay;
+        public final ForgeConfigSpec.BooleanValue warriorsHaveSwords;
+        public final ForgeConfigSpec.BooleanValue minersHavePickaxes;
+        public final ForgeConfigSpec.BooleanValue hazmatSuitProtectsFromPotions;
 
         General(ForgeConfigSpec.Builder builder) {
             builder.push("General");
@@ -49,9 +163,6 @@ public class ConfigHandler {
             hazmatSuitProtectsFromPotions = builder
                     .comment("Are hazmat zombies immune to potions?")
                     .define("hazmatSuitProtectsFromPotions", true);
-            chestheadLoot = builder
-                    .comment("Possible chesthead loot")
-                    .defineList("chestheadLoot", new ArrayList<String>(), o -> itemExists(o.toString()));
 
             builder.pop();
         }
@@ -185,6 +296,65 @@ public class ConfigHandler {
             maxGroupZombieBear = builder
                     .comment("Zombie bear max count in group, 0-16:")
                     .defineInRange("maxGroupZombieBear", 3, 0, 16);
+            builder.pop();
+        }
+    }
+
+    public static class Senses {
+        public final ForgeConfigSpec.ConfigValue<List<? extends String>> blockBreak;
+        public final ForgeConfigSpec.IntValue blockBreakNotifyRadius;
+        public final ForgeConfigSpec.ConfigValue<List<? extends String>> blockPlace;
+        public final ForgeConfigSpec.IntValue blockPlaceNotifyRadius;
+        public final ForgeConfigSpec.IntValue explosionNotifyRadius;
+        public final ForgeConfigSpec.IntValue anvilNotifyRadius;
+        public final ForgeConfigSpec.IntValue noteblockNotifyRadius;
+        //public final ForgeConfigSpec.IntValue minecartNotifyRadius;
+
+        public static boolean blockExists(String ID) {
+            String id_parts[] = ID.split(":");
+            if(id_parts.length <= 1 || id_parts.length > 3) { return false; }
+            if(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(id_parts[0], id_parts[1])) == null) {return false;}
+            return true;
+        }
+
+
+        Senses(ForgeConfigSpec.Builder builder) {
+            builder.push("Senses");
+            System.out.println("Creating lists...");
+            ArrayList<String> blocks_break = new ArrayList<String>();
+            ArrayList<String> blocks_place = new ArrayList<String>();
+            System.out.println("Populating lists...");
+            blocks_break.add("minecraft:glass");
+            blocks_place.add("minecraft:torch");
+
+            System.out.println("Setting up \"blockBreak\"...");
+            blockBreak = builder
+                    .comment("Zombies will be attracted to breaking these blocks:")
+                    .defineList("blockBreak", blocks_break, o -> blockExists(o.toString()));
+            System.out.println("Done!");
+            blockBreakNotifyRadius = builder
+                    .comment("Radius of area around the player where zombies will be notified about breaking blocks:")
+                    .defineInRange("blockBreakNotifyRadius", 64, 0, Integer.MAX_VALUE);
+            blockPlace = builder
+                    .comment("Zombies will be attracted to placing these blocks:")
+                    .defineList("blockPlace", blocks_place, o -> blockExists(o.toString()));
+            blockPlaceNotifyRadius = builder
+                    .comment("Radius of area around the player where zombies will be notified about placing blocks:")
+                    .defineInRange("blockPlaceNotifyRadius", 64, 0, Integer.MAX_VALUE);
+            explosionNotifyRadius = builder
+                    .comment("Radius of area around the player where zombies will be notified about explosions:")
+                    .defineInRange("explosionNotifyRadius", 128, 0, Integer.MAX_VALUE);
+            anvilNotifyRadius = builder
+                    .comment("Radius of area where zombies will be notified about using anvil:")
+                    .defineInRange("anvilNotifyRadius", 64, 0, Integer.MAX_VALUE);
+            noteblockNotifyRadius = builder
+                    .comment("Radius of area where zombies will be notified about noteblock activation:")
+                    .defineInRange("noteblockNotifyRadius", 64, 0, Integer.MAX_VALUE);
+            /*
+            minecartNotifyRadius = builder
+                    .comment("Radius of area where zombies will be notified about moving minecart:")
+                    .defineInRange("minecartNotifyRadius", 64, 0, Integer.MAX_VALUE);
+            */
             builder.pop();
         }
     }
