2bad2d4726ac45b0d1c1524d7f43e05115add7cb
MadAlchemist <42516313+MadAlchemist@users.noreply.github.com>
2021-11-09 22:11:19 +0300

    Create ModdedNearestAttackableTargetGoal.java

---
+++ src/main/java/com/madalchemist/zombienation/zombies/ModdedNearestAttackableTargetGoal.java
@@ -0,0 +1,79 @@
+package com.madalchemist.zombienation.zombies;
+
+import java.util.EnumSet;
+import java.util.function.Predicate;
+import javax.annotation.Nullable;
+import net.minecraft.entity.EntityPredicate;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.ai.goal.TargetGoal;
+import net.minecraft.entity.monster.DrownedEntity;
+import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.passive.DolphinEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+
+public class ModdedNearestAttackableTargetGoal<T extends LivingEntity> extends TargetGoal {
+   protected final Class<T> targetType;
+   protected final int randomInterval;
+   protected LivingEntity target;
+   protected EntityPredicate targetConditions;
+
+   public ModdedNearestAttackableTargetGoal(MobEntity p_i50313_1_, Class<T> p_i50313_2_, boolean p_i50313_3_) {
+         this(p_i50313_1_, p_i50313_2_, p_i50313_3_, false);
+   }
+
+   public ModdedNearestAttackableTargetGoal(MobEntity p_i50314_1_, Class<T> p_i50314_2_, boolean p_i50314_3_, boolean p_i50314_4_) {
+      this(p_i50314_1_, p_i50314_2_, 10, p_i50314_3_, p_i50314_4_, (Predicate<LivingEntity>)null);
+   }
+
+   public ModdedNearestAttackableTargetGoal(MobEntity p_i50315_1_, Class<T> p_i50315_2_, int p_i50315_3_, boolean p_i50315_4_, boolean p_i50315_5_, @Nullable Predicate<LivingEntity> p_i50315_6_) {
+      super(p_i50315_1_, p_i50315_4_, p_i50315_5_);
+      this.targetType = p_i50315_2_;
+      this.randomInterval = p_i50315_3_;
+      this.setFlags(EnumSet.of(Goal.Flag.TARGET));
+      this.targetConditions = (new EntityPredicate()).range(this.getFollowDistance()).selector(p_i50315_6_);
+   }
+
+   public boolean canUse() {
+      if (this.randomInterval > 0 && this.mob.getRandom().nextInt(this.randomInterval) != 0) {
+         return false;
+      } else {
+         this.findTarget();
+         return this.target != null;
+      }
+   }
+
+   protected AxisAlignedBB getTargetSearchArea(double p_188511_1_) {
+      return this.mob.getBoundingBox().inflate(p_188511_1_, 4.0D, p_188511_1_);
+   }
+
+   protected void findTarget() {
+      if (this.targetType != PlayerEntity.class && this.targetType != ServerPlayerEntity.class) {
+         this.target = this.mob.level.getNearestLoadedEntity(this.targetType, this.targetConditions, this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ(), this.getTargetSearchArea(this.getFollowDistance()));
+      } else {
+         this.target = this.mob.level.getNearestPlayer(this.targetConditions, this.mob, this.mob.getX(), this.mob.getEyeY(), this.mob.getZ());
+      }
+
+   }
+
+   public void start() {
+      if(this.mob instanceof ZolphinEntity){
+         if(this.target instanceof ZolphinEntity || this.target instanceof ZombieEntity) {
+            this.target = null;
+         }
+      }
+      if(this.mob instanceof DrownedEntity){
+         if(this.target instanceof ZolphinEntity || this.target instanceof ZombieEntity) {
+            this.target = null;
+         }
+      }
+      this.mob.setTarget(this.target);
+   }
+
+   public void setTarget(@Nullable LivingEntity p_234054_1_) {
+      this.target = p_234054_1_;
+   }
+}
